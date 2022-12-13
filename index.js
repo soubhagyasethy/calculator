3b795286d4fa7f63785bc7633f97e53e1a746904
Soubhagya Sethy <ssethy017@gmail.com>
2022-12-13 19:38:10 +0530

    final commit

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ let buttons = document.querySelectorAll('.button');
 
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click', (e)=>{
-        if (e.target.innerHTML == '='){
+        if (e.target.innerHTML == '='){  // logic for "=" operator
             string = eval(string);
             document.querySelector('input').value = string;
         }
-        else if (e.target.innerHTML == 'C'){
+        else if (e.target.innerHTML == 'C'){ //logic for C button
             string = "";
             document.querySelector('input').value = string;
         }
-        else if (e.target.innerHTML == 'AC'){
+        else if (e.target.innerHTML == 'AC'){ //logic for AC button
             string = "";
             document.querySelector('input').value = string;
         }
